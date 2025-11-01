d13ed2e11531710568f8d582c43a10311ef98877
ManjuGJ <98093288+ManjuGJ@users.noreply.github.com>
2025-11-01 12:41:38 +0530

    Update CORS origin URLs for production

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-     origin:["http://localhost:5173" , "http://localhost:5174"],
+     origin:["https://vandycart-ui.onrender.com" , "https://vandycart-admin.onrender.com"],
  credentials:true
 }))
 app.use("/api/auth", authRoutes)
